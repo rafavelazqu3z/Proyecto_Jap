ad697da6341b3ba119aec70243e55736b71e46ad
rafavelazqu3z <106779880+rafavelazqu3z@users.noreply.github.com>
2022-08-11 09:23:17 -0300

    Login+Google,productos-autos,

---
+++ js/products.js
@@ -0,0 +1,52 @@
+let categoriesArray = [];
+
+//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.products.length; i++){ 
+        let category = array.products[i];
+            console.log(category);
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                <img src="` + category.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.name +" "+"- "+category.currency+" "+ category.cost+`</h4> 
+                        <p> `+ category.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + category.soldCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("listado_auto").innerHTML = htmlContentToAppend; 
+    }
+    document.getElementById("descripcion_cat").innerHTML = "Verás aqui todos los productos de la categoria "+ array.catName; 
+}
+
+
+/* 
+EJECUCIÓN:
+
+-Al cargar la página se llama a getJSONData() pasándole por parámetro la dirección para obtener el listado.
+-Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en categoriesArray.
+-Por último, se llama a showCategoriesList() pasándole por parámetro categoriesArray.
+
+*/
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(LIST_AUTO).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            showCategoriesList(categoriesArray);
+        }
+    });
+});
