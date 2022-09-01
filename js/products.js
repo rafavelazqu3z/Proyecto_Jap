f4b04d64149a82102f68a9bc916d81eaa3a5eb6c
rafavelazqu3z <106779880+rafavelazqu3z@users.noreply.github.com>
2022-09-01 11:01:29 -0300

    update de products

--- js/products.js
+++ js/products.js
@@ -1,50 +1,63 @@
-const ORDER_ASC_BY_NAME = "AZ";
-const ORDER_DESC_BY_NAME = "ZA";
-const ORDER_BY_PROD_COUNT = "Cant.";
-let currentCategoriesArray = [];
-let currentSortCriteria = undefined;
-let minCount = undefined;
-let maxCount = undefined;
-let categoryName;
+let rangeMin = document.getElementById("rangeFilterCountMin")
+let rangeMax = document.getElementById("rangeFilterCountMax")
+let searchBar = document.getElementById('busqueda')
+let productList = ""
 
 
-function sortCategories(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.cost < b.cost ){ return -1; }
-            if ( a.cost > b.cost ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC_BY_NAME){
-        result = array.sort(function(a, b) {
-            if ( a.cost > b.cost ){ return -1; }
-            if ( a.cost < b.cost ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_BY_PROD_COUNT){
-        result = array.sort(function(a, b) {
-            let aCount = parseInt(a.soldCount);
-            let bCount = parseInt(b.soldCount);
+function sortByPriceDesc(a,b){
+    return a.cost - b.cost  
+}
 
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
-            return 0;
-        });
-    }
+function sortByPriceAsc(a,b){
+    return b.cost - a.cost
+}
 
-    return result;
+function sortByRel(a,b){
+    return b.soldCount - a.soldCount
+}
+
+function productSortMore(){
+    showCategoriesList(productList.sort(sortByPriceDesc));
+}
+
+function productSortLess(){
+    showCategoriesList(productList.sort(sortByPriceAsc));
+}
+
+function productSortRel(){
+    showCategoriesList(productList.sort(sortByRel));
+}
+
+function productsClean(){
+    showCategoriesList(productList);
+    document.getElementById("rangeFilterCountMin").value = undefined;
+    document.getElementById("rangeFilterCountMax").value = undefined;
+    search()
+}
+
+
+function searchAndFilter(){
+    let filteredProducts = productList;
+    
+    if(rangeMin.value != ""){
+        filteredProducts = filteredProducts.filter(product => 
+        product.cost > rangeFilterCountMin.value)};
+    if(rangeMax.value != ""){
+        filteredProducts = filteredProducts.filter(product => 
+        product.cost < rangeFilterCountMax.value)};
+    if(searchBar.value != ""){
+        filteredProducts = filteredProducts.filter(product => 
+        product.name.toLowerCase().includes(busqueda.value.toLowerCase())|| product.description.toLowerCase().includes(busqueda.value.toLowerCase()))}
+
+    showCategoriesList(filteredProducts)
 }
 
 //función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
-function showCategoriesList(){
+function showCategoriesList(list){
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentCategoriesArray.length; i++){ 
-        let category = currentCategoriesArray[i];
+    for(let i = 0; i < list.length; i++){ 
+        let category = list[i];
         
-        if (((minCount == undefined) || (minCount != undefined && parseInt(category.cost) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.cost) <= maxCount))){
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
@@ -64,25 +77,11 @@ function showCategoriesList(){
             </div>
         </div>
         `
-        }
+        
 
         document.getElementById("listado_auto").innerHTML = htmlContentToAppend; 
     }
-    document.getElementById("descripcion_cat").innerHTML = "Verás aqui todos los productos de la categoria "+ categoryName; 
-}
-
-
-function sortAndShowCategories(sortCriteria, categoriesArray){
-    currentSortCriteria = sortCriteria;
-
-    if(categoriesArray != undefined){
-        currentCategoriesArray = categoriesArray;
-    }
-
-    currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
-
-    //Muestro las categorías ordenadas
-    showCategoriesList();
+    document.getElementById("descripcion_cat").innerHTML = "Verás aqui todos los productos de la categoria "+ list.catName; 
 }
 
 
@@ -95,131 +94,16 @@ EJECUCIÓN:
 
 */
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            currentCategoriesArray = resultObj.data.products;
-            categoryName = resultObj.data.catName;
-            showCategoriesList();
-        }
-    });
-    
-    document.getElementById("sortAsc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_ASC_BY_NAME);
+    document.addEventListener("DOMContentLoaded", function(e){
+        getJSONData(PRODUCTS_URL).then(function(resultObj){
+            if (resultObj.status === "ok")
+            {
+                productList = resultObj.data.products;
+                showCategoriesList(productList);
+            }
+        });
     });
 
-    document.getElementById("sortDesc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_DESC_BY_NAME);
-    });
-
-    document.getElementById("sortByCount").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_BY_PROD_COUNT);
-    });
-
-    document.getElementById("clearRangeFilter").addEventListener("click", function(){
-        document.getElementById("rangeFilterCountMin").value = "";
-        document.getElementById("rangeFilterCountMax").value = "";
-        document.getElementById("busqueda").value = "";
-
-        minCount = undefined;
-        maxCount = undefined;
-
-        showCategoriesList();
-    });
-
-    document.getElementById("rangeFilterCount").addEventListener("click", function(){
-        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
-        //de productos por categoría.
-        minCount = document.getElementById("rangeFilterCountMin").value;
-        maxCount = document.getElementById("rangeFilterCountMax").value;
-
-        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
-            minCount = parseInt(minCount);
-        }
-        else{
-            minCount = undefined;
-        }
-
-        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
-            maxCount = parseInt(maxCount);
-        }
-        else{
-            maxCount = undefined;
-        }
-
-        showCategoriesList();
-    });
-});
-
-let buscar = document.getElementById('busqueda');
-
-buscar.addEventListener('keyup', (event) => {
-    let busqueda = buscar.value.toLowerCase();
-    let htmlContentToAppend = "";
-    for (let i = 0; i < currentCategoriesArray.length; i++) {
-        let descripcion = currentCategoriesArray[i].description.toLowerCase();
-        let category = currentCategoriesArray[i];
-        let nombre = currentCategoriesArray[i].name.toLowerCase();
-        if(nombre.indexOf(busqueda) !== -1){
-            htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                <img src="` + category.image + `" alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4>`+ category.name +" "+"- "+category.currency+" "+ category.cost+`</h4> 
-                        <p> `+ category.description +`</p> 
-                        </div>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small> 
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        `
-        }else if(descripcion.indexOf(busqueda) !== -1){
-            htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                <img src="` + category.image + `" alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4>`+ category.name +" "+"- "+category.currency+" "+ category.cost+`</h4> 
-                        <p> `+ category.description +`</p> 
-                        </div>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small> 
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        `
-        }else{
-            htmlContentToAppend = `
-        <div class="list-group-item">
-            <div class="row ">
-                
-                 <center><h5 class="m-3">No se encontro ningun vehiculo</h5></center>  
-
-                
-            </div>
-        </div>
-        `
-        }
-
-        
-
-        document.getElementById("listado_auto").innerHTML = htmlContentToAppend; 
-        
-        
-    }
-
-    
-});
+    document.getElementById('busqueda').addEventListener('input', searchAndFilter);
+    document.getElementById("rangeFilterCountMin").addEventListener('input', searchAndFilter);
+    document.getElementById("rangeFilterCountMax").addEventListener('input', searchAndFilter);
